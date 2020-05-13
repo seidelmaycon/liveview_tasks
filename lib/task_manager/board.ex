f61d5100051149cc1c081ac5ee6ddeaba5196d40
mayconseidel <maycon.seidel@gmail.com>
2020-05-13 15:46:03 -0300

    Add ability to delete tasks

--- lib/task_manager/board.ex
+++ lib/task_manager/board.ex
@@ -10,48 +10,14 @@ defmodule TaskManager.Board do
 
   @topic inspect(__MODULE__)
 
-  @doc """
-  Returns the list of tasks.
-
-  ## Examples
-
-      iex> list_tasks()
-      [%Task{}, ...]
-
-  """
+  @spec list_tasks :: any
   def list_tasks do
     from(t in Task, order_by: t.done)
     |> Repo.all()
   end
 
-  @doc """
-  Gets a single task.
-
-  Raises `Ecto.NoResultsError` if the Task does not exist.
-
-  ## Examples
-
-      iex> get_task!(123)
-      %Task{}
-
-      iex> get_task!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_task!(id), do: Repo.get!(Task, id)
 
-  @doc """
-  Creates a task.
-
-  ## Examples
-
-      iex> create_task(%{field: value})
-      {:ok, %Task{}}
-
-      iex> create_task(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_task(attrs \\ %{}) do
     %Task{}
     |> Task.changeset(attrs)
@@ -59,18 +25,6 @@ defmodule TaskManager.Board do
     |> broadcast_change([:task, :created])
   end
 
-  @doc """
-  Updates a task.
-
-  ## Examples
-
-      iex> update_task(task, %{field: new_value})
-      {:ok, %Task{}}
-
-      iex> update_task(task, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_task(%Task{} = task, attrs) do
     task
     |> Task.changeset(attrs)
@@ -78,32 +32,11 @@ defmodule TaskManager.Board do
     |> broadcast_change([:task, :updated])
   end
 
-  @doc """
-  Deletes a task.
-
-  ## Examples
-
-      iex> delete_task(task)
-      {:ok, %Task{}}
-
-      iex> delete_task(task)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_task(%Task{} = task) do
     Repo.delete(task)
     |> broadcast_change([:task, :deleted])
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking task changes.
-
-  ## Examples
-
-      iex> change_task(task)
-      %Ecto.Changeset{data: %Task{}}
-
-  """
   def change_task(%Task{} = task, attrs \\ %{}) do
     Task.changeset(task, attrs)
   end
